14bce309781bc5cb5f186681f39ffef4117ba333
Mansi-unge <mansiunge@gmail.com>
2025-06-02 21:19:05 +0530

    UPDATE REPO

---
+++ Backend/Server.js
@@ -0,0 +1,28 @@
+import express from "express" ;
+import cors from "cors" ; 
+import dotenv from "dotenv" ;
+import connectDB from "./config/db.js";
+
+
+dotenv.config();
+
+const app = express() ;
+
+connectDB();
+
+//middleware 
+app.use(cors());
+app.use(express.json());
+
+
+//getting port from the env file 
+const PORT = process.env.PORT || 8000 ;
+
+app.get("/" , (req , res) => {
+  console.log("server is running");
+})
+
+app.listen(PORT , () => {
+  
+  console.log(`server is running on localhost port number : ${PORT}`)
+})
---
+++ Backend/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGODB_URI;
+
+const connectDB = async () => {
+  try{
+    await mongoose.connect(MONGO_URI);
+    console.log("Server connected to database........")
+  }catch(error){
+    console.log("error in connecting database to server ");
+    process.exit(1);
+  }
+}
+
+export default connectDB;
