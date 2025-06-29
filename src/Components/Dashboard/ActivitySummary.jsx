4909c7ad4ffa827f9a5e4f457eb2529f1f8d3fa0
Mansi Unge <mansiunge@gmail.com>
2025-06-29 16:42:27 +0530

    UPDATE REPO

--- src/Components/Dashboard/ActivitySummary.jsx
+++ src/Components/Dashboard/ActivitySummary.jsx
@@ -88,12 +88,15 @@ const ActivitySummary = ({ recentActivity }) => {
                 axisLine={false}
                 tickLine={false}
               />
-              <YAxis
-                allowDecimals={false}
-                tick={{ fontSize: 12, fill: "#4b5563" }}
-                axisLine={false}
-                tickLine={false}
-              />
+              {!isMobile && (
+  <YAxis
+    allowDecimals={false}
+    tick={{ fontSize: 12, fill: "#4b5563" }}
+    axisLine={false}
+    tickLine={false}
+  />
+)}
+
               <RechartsTooltip content={<CustomTooltip />} cursor={{ fill: "transparent" }} />
               {!isMobile && <Legend verticalAlign="top" height={36} />}
 
