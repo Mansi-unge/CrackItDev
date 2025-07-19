690a7961b01cfc40692bad0587284d35d0596dbd
MANSI UNGE <mansiunge@gmail.com>
2025-07-19 13:32:19 +0530

    Update Header.jsx

--- src/Components/Common/Header.jsx
+++ src/Components/Common/Header.jsx
@@ -150,7 +150,8 @@ const Header = () => {
             >
               Login
             </Link>
-            <Link to="/signup" state={{ tab: "signup" }}>
+            <Link to="/signup" state={{ tab: "Sign
+            up" }}>
               <button className="bg-blue-600 text-white px-4 py-2 rounded-xl text-sm font-medium hover:bg-blue-700 transition">
                 Sign up
               </button>
